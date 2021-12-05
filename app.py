1d95db916cb7e9f96ca115b101ce1ecaf8f86a6f
stanley-chou <68239230+stanley-chou@users.noreply.github.com>
2021-12-04 23:46:54 -0500

    Log In and Registration are Completely Done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, url_for, redirect#import flask into this file and render_template
 import random
 
+import PIL
 import json
 from datetime import datetime
 import mysql.connector #import SQL
@@ -52,8 +53,21 @@ def login(): #return some object to be displayed to the user | general python sy
 
 
 
-@app.route('/login_page')
+@app.route('/login_page', methods=['GET', 'POST'])
 def login_page(): #name of function and name of route do not have to match
+    if request.method == 'POST':
+        name = request.form.get('UserName')
+        password = request.form.get('Password')
+        mycursor.execute("SELECT username, user_password, User_ID FROM User_Profile WHERE username = %s and user_password = %s;", (name,password))
+        result = mycursor.fetchone()
+        global currentUser
+        if (result == None):
+            return redirect(url_for('login_page'))
+        if(name == result[0] and password == result[1]):
+            currentUser = result[2]
+            return redirect(url_for('select'))
+        else:
+            return redirect(url_for('login_page'))    
     return render_template('login.html')
 
 @app.route('/services')
@@ -85,62 +99,39 @@ def chat(): #name of function and name of route do not have to match
 def registration_form():
     if request.method == 'POST':
         name = request.form.get('username')
+        #print(name)
         password = request.form.get('password')
-        street = request.form.get('street')
-        city = request.form.get('city')
-        state = request.form.get('state')
-        country = request.form.get('country')
-        zipcode = request.form.get('zipcode')
-        phonenum = request.form.get('phoneNum')
-        emailaddress = request.form.get('emailAddress')
-        creditcard = request.form.get('creditCard')
+        #print(password)
+        age = request.form.get('age')
+        #print(age)
+        campus = request.form.get('Campus')
+        #print(campus)
+        residence = request.form.get('Residence_Hall')
+        #print(residence)
+        gender = request.form.get('gender')
+        #print(gender)
+        year = request.form.get('year')
+        #print(year)
+        orientation = request.form.get('sexual_orientation')
+        #print(orientation)
+        major = request.form.get('major')
+        #print(major)
+        school = request.form.get('school')
+        #print(school)
+        bio = request.form.get('bio')
+        #print(bio)
         userID = random.randint(0, 1000000)
-        mycursor.execute("INSERT INTO User_Profile(User_ID,city,state,street,country,zip_code,user_password,username,phoneNum,email_address,credit_card_number) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-         (userID, city, state, street, country, zipcode, password, name, phonenum,emailaddress,creditcard))
+        #print(userID)
+        img = request.form.get('profile_picture')
+        #print(img)
+        imagefile = request.files.get('profile_picture', '')
+        mycursor.execute("INSERT INTO User_Profile(User_ID,username,user_password,campus,residence,age,gender,year,sexual_orientation, major, bio, school, profile_picture ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        (userID, name, password, campus, residence, age, gender, year, orientation,major, bio, school, imagefile))
         mydb.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('login_page'))
     return render_template('registration_form.html')
 
-@app.route('/Restaurant', methods=['GET', 'POST'])
-def dropdown():
-    if request.method == 'POST':
-        rest = request.form.get("Restaurants")
-        global currentRestaurant
-        mycursor.execute("SELECT RestaurantID FROM Restaurant where RestaurantName = '"+ rest +"';")
-        result = mycursor.fetchone()    
-        currentRestaurant = result[0]
-        print(currentRestaurant)
-        return redirect(url_for('menu'))
-    mycursor.execute("select  RestaurantName from Restaurant;")
-    result = mycursor.fetchall()
-    rows=[i[0] for i in result]
-    return render_template('dropdown.html', content = rows)
 
-@app.route('/menu', methods=['GET', 'POST']) #someone has visited base url and we have to provide information
-def menu(): #return some object to be displayed to the user | general python syntax for defining a function
-    if request.method == 'POST':
-
-        return redirect(url_for('orderStats'))
-    mycursor.execute("select  Dish_Name from foodMenu where RestaurantID = '"+ str(currentRestaurant) +"';")
-    result = mycursor.fetchall()
-    rows=[i[0] for i in result]
-    mycursor.execute("select Dish_Name, Price from foodMenu where RestaurantID = '"+ str(currentRestaurant) +"';")
-    result2 =mycursor.fetchall()
-    rows2 =  result2
-    return render_template('menu.html', content = rows, prices = rows2) #name='Irfan'
-
-@app.route('/orderStats', methods=['GET', 'POST']) #someone has visited base url and we have to provide information
-def orderStats(): #return some object to be displayed to the user | general python syntax for defining a function
-    if request.method == 'POST':
-        return redirect(url_for('login'))
-    global currentUser, currentRestaurant
-    mycursor.execute("select street, city, state, country, zip_code from Restaurant where RestaurantID = '"+ str(currentRestaurant) +"';")
-    restaurant_loc = ' '.join(mycursor.fetchall()[0])
-    mycursor.execute("select street, city, state, country, zip_code from User_Profile where User_ID = '"+ str(currentUser) +"';")
-    user_loc = ' '.join(mycursor.fetchall()[0])
-    time_to_deliver = ceil(optimize_delivery(restaurant_loc, user_loc)/60)
-    print(restaurant_loc, user_loc, time_to_deliver)
-    return render_template('orderStats.html', delivery_time=time_to_deliver) #name='Irfan'
 
 if __name__ == "__main__":
     app.run()
