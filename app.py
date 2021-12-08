4b4e639fce6511621899f3f04b61bf8fc61c7e2f
stanley-chou <68239230+stanley-chou@users.noreply.github.com>
2021-12-08 02:13:27 -0500

    fixing bugs

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from datetime import datetime
 import mysql.connector #import SQL
 from math import ceil
 
+
+
 def optimize_delivery(curr_r, curr_c):
 
     with open("api_key.txt") as f:
@@ -34,14 +36,21 @@ def optimize_delivery(curr_r, curr_c):
     return total_time
 
 
-mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "", database ="RUDownBad_Database") #connect to database, bad practive since problems with multiple connecctions and errors can occur, but fine for single user only
+mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "3072", database ="RUDownBad_Database") #connect to database, bad practive since problems with multiple connecctions and errors can occur, but fine for single user only
 mycursor = mydb.cursor()
 
 
-restaurants = ["Applebees", "Olive Garden", "McDonalds", "Popeyes"]
 
 currentRestaurant = None #will update one a Restaurant is selected
 currentUser = None #will update when  a user logs in
+globalCampus = None
+globalYear = None
+globalSex = None
+globalSchool = None
+globalGender = None
+
+globalCurrOtherUserID = None
+globalCurrInfo = None
 
 app = Flask(__name__)
 
@@ -49,6 +58,8 @@ print(__name__)
 
 @app.route('/', methods=['GET', 'POST']) #someone has visited base url and we have to provide information
 def login(): #return some object to be displayed to the user | general python syntax for defining a function
+    global currentUser
+    currentUser = None
     return render_template('home.html') #name='Irfan'
 
 
@@ -89,11 +100,6 @@ def select(): #name of function and name of route do not have to match
 @app.route('/filter', methods=['GET', 'POST'])
 def filter(): #name of function and name of route do not have to match
     if request.method == 'POST':
-        age = request.form.get('age')
-        print(age)
-        if age == '':
-            age = 'age'
-        print(age)
         campus = request.form.get('Campus')
         print(campus)
         gender = request.form.get('gender')
@@ -106,12 +112,48 @@ def filter(): #name of function and name of route do not have to match
         print(sex)
         school = request.form.get('school')
         print(school)
-        sql_select_query = 'SELECT username, campus, residence, age, gender year, sexual_orientation, major, bio, school FROM user_profile WHERE age = ' + str(age) + ' and campus = "' + str(campus) + '" and gender = "' + str(gender) + '" and year = "' + str(year) + '" and sexual_orientation = "' + str(sex) + '" and school = "' + str(school) + '" ORDER BY RAND() LIMIT1;'
+        sql_select_query = 'SELECT username, campus, residence, age, gender, year, sexual_orientation, major, bio, school, User_ID FROM user_profile WHERE campus = "' + str(campus) + '" and gender = "' + str(gender) + '" and year = "' + str(year) + '" and sexual_orientation = "' + str(sex) + '" and school = "' + str(school) + '" ORDER BY RAND() LIMIT 1;'
         print(sql_select_query)
         mycursor.execute(sql_select_query)
         result = mycursor.fetchall()
         print(result)
+        global globalCampus 
+        globalCampus = campus
+        global globalYear 
+        globalYear = year
+        global globalSex 
+        globalSex = sex
+        global globalSchool
+        globalSchool = school
+        global globalGender 
+        globalGender = gender 
+        global globalCurrInfo 
+        globalCurrInfo = result
+        OtherID = None
+        OtherID = result[0][10]
+        print(OtherID)
+        global globalCurrOtherUserID
+        globalCurrOtherUserID = OtherID
+        #saving all this information for use in the next page
+        return redirect(url_for('profile_page'))  
     return render_template('filter.html')
+
+
+@app.route('/profiles',  methods=['GET', 'POST'])
+def profile_page(): #name of function and name of route do not have to match
+    global globalCurrInfo
+    profileInfo = globalCurrInfo
+    global globalCurrOtherUserID
+    currID = globalCurrOtherUserID
+    sql_select_query = 'SELECT profile_picture FROM user_profile WHERE User_ID = "' + str(currID) + '" ;'
+    print(sql_select_query)
+    mycursor.execute(sql_select_query)
+    result = mycursor.fetchall()
+    profile_picture = result[0][0]
+    print(profile_picture)
+    return render_template('profiles.html', content = profileInfo )
+
+
 
 @app.route('/chat')
 def chat(): #name of function and name of route do not have to match
@@ -148,6 +190,7 @@ def registration_form():
         img = request.form.get('profile_picture')
         print(img)
         imagefile = request.files.get('profile_picture', '')
+        print(imagefile)
         mycursor.execute("INSERT INTO User_Profile(User_ID,username,user_password,campus,residence,age,gender,year,sexual_orientation, major, bio, school, profile_picture ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
         (userID, name, password, campus, residence, age, gender, year, orientation,major, bio, school, imagefile))
         mydb.commit()
@@ -158,4 +201,3 @@ def registration_form():
 
 if __name__ == "__main__":
     app.run()
-
